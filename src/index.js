f27856a7447789f0da9e2698b712fef4dc629d1b
Murilo M. Grosso <murilogrosso@hotmail.com>
2024-03-26 10:15:02 -0300

    Main Page Update

--- src/index.js
+++ src/index.js
@@ -7,4 +7,22 @@ $(document).ready(function()
     document.getElementById('monkFormButton').addEventListener('click', function(){
         window.location.href = "photo";
     });
+
+    document.getElementById('summaryButton').addEventListener('click', function(){
+        scrollTo("summary");
+    });
+
+    document.getElementById('formsButton').addEventListener('click', function(){
+        scrollTo("forms");
+    });
+
+    document.getElementById('contactButton').addEventListener('click', function(){
+        scrollTo("contact");
+    });
 })
+
+function scrollTo(id)
+{
+    var scrollDiv = document.getElementById(id).offsetTop;
+    window.scrollTo({ top: scrollDiv -50, behavior: 'smooth'});
+}
